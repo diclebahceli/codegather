9ea04e969dc063ac5a47b80db2904a1615b994c8
yigitkalan <sametykalan@gmail.com>
2024-03-02 22:46:55 +0300

    feat: update competition card

--- Frontend/codegather/src/app/pages/ongoingCompetitions/page.tsx
+++ Frontend/codegather/src/app/pages/ongoingCompetitions/page.tsx
@@ -7,8 +7,8 @@ import { useState } from "react";
 const ongoingCompetitions = () => {
     const [competitions, setCompetitions] = useState<Competition[]>(getAllCompetitions);
     return (
-        <div className="vh-100">
-            <h1 className="fs-1 mt-5 container">ONGOING COMPETITIONS</h1>
+        <div className="h-100 bg-theme-darker" style={{backgroundAttachment: 'fixed'}}>
+            <h1 className="fs-1 pt-5 container">ONGOING COMPETITIONS</h1>
             <div className="d-flex flex-row justify-content-evenly">
                 <div className="mt-5 ms-5 ps-5 d-flex flex-wrap  col-7">
                     {competitions.map((competition, index) => (
