5dbcdbaff01bf7fe45a7b940225935589d2de31e
yigitkalan <sametykalan@gmail.com>
2024-03-13 01:17:22 +0300

    style: minor tweaks on pages

--- FRONTEND/src/app/components/header/Header.tsx
+++ FRONTEND/src/app/components/header/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
     return (
-        <header className=" bg-darker p-3 d-flex align-items-center" style={{ height: "4.5rem" }}>
+        <header className=" bg-darker p-3 d-flex flex-row align-items-center justify-content-center" style={{ height: "4.5rem" }}>
             <a href="" className="  fs-2">
                 Code Gather
             </a>
