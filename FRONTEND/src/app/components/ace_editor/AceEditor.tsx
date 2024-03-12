f1a4362c2ec542f9146c237e5ca1f502ee863e58
yigitkalan <sametykalan@gmail.com>
2024-03-12 20:58:41 +0300

    chore: move file structure

---
+++ FRONTEND/src/app/components/ace_editor/AceEditor.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import AceEditor from "react-ace";
+
+import "ace-builds/src-noconflict/ace";
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/theme-github";
+import "ace-builds/src-noconflict/ext-language_tools";
+import { Ace } from "ace-builds";
+
+
+function AceEditorComponent({onValueChange}: {onValueChange: (value: string) => void}) {
+    function onChange(newValue: string) {
+            onValueChange(newValue);
+    }
+    function onLoad(editor: Ace.Editor) {
+        console.log("i've loaded");
+    }
+    return (
+        <AceEditor
+            placeholder="Placeholder Text"
+            mode="javascript"
+            theme="solarized_dark"
+            name="blah2"
+            onLoad={onLoad}
+            onChange={onChange}
+            fontSize={18}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            value={`function onLoad(editor) {
+  console.log("i've loaded");
+}`}
+            setOptions={{
+                enableBasicAutocompletion: true,
+                enableLiveAutocompletion: false,
+                enableSnippets: false,
+                showLineNumbers: true,
+                tabSize: 2,
+            }} />
+
+    );
+}
+
+export default AceEditorComponent;
