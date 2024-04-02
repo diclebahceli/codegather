2c155bc42ee7bf3ad639493ad9a77eb7d0f45285
diclebahceli <diclebahceli24@gmail.com>
2024-04-02 18:24:28 +0300

    feat: add controller for authentication

---
+++ BACKEND/Presentation/codegather.Api/Controllers/AuthController.cs
@@ -0,0 +1,25 @@
+﻿using codegather.Application;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace codegather.Api;
+
+
+[Route("api/[controller]/[action]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    private readonly IMediator mediator;
+
+    public AuthController(IMediator mediator)
+    {
+        this.mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Register(RegisterCommandRequest request)
+    {
+        await mediator.Send(request);
+        return StatusCode(StatusCodes.Status201Created);
+    }
+}
