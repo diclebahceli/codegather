8abeca996639facb74cd52f5d35bc460d378c2f2
diclebahceli <diclebahceli24@gmail.com>
2024-04-12 20:12:34 +0300

    fix: change id to guid

--- BACKEND/Core/codegather.Domain/Entities/Question.cs
+++ BACKEND/Core/codegather.Domain/Entities/Question.cs
@@ -2,7 +2,7 @@ namespace codegather.Domain;
 public class Question: EntityBase
 {
     public Competition Competition {get; set;}
-    public int CompetitionId {get; set;}
+    public Guid CompetitionId {get; set;}
     public ICollection<Submission> Submissions {get; set;}
     public string Description {get; set;}
     public string TestCases {get; set;}
