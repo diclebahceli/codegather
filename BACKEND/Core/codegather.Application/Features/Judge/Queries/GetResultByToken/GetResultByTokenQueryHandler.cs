5faf5b379afaf89433a2896a99c9567e0295f2e7
diclebahceli <diclebahceli24@gmail.com>
2024-04-13 18:41:06 +0300

    feat: create a query for taking results from the api

---
+++ BACKEND/Core/codegather.Application/Features/Judge/Queries/GetResultByToken/GetResultByTokenQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using codegather.Application.Interfaces.AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace codegather.Application;
+
+public class GetResultByTokenQueryHandler : BaseHandler, IRequestHandler<GetResultByTokenQueryRequest, GetResultByTokenQueryResponse>
+{
+    ICodeEditorService codeEditorService;
+    public GetResultByTokenQueryHandler(ICodeEditorService codeEditorService, IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+    {
+        this.codeEditorService = codeEditorService;
+    }
+
+    public async Task<GetResultByTokenQueryResponse> Handle(GetResultByTokenQueryRequest request, CancellationToken cancellationToken)
+    {
+        var result = await codeEditorService.GetResult(request.Token);
+        if (result == null)
+            throw new Exception("No such submission found");
+
+        return new GetResultByTokenQueryResponse
+        {
+            Submission = result
+        };
+    }
+}
