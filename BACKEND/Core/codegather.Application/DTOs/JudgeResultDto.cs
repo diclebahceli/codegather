251fffa0fecec7d168f018c3d18f9e253db221df
diclebahceli <diclebahceli24@gmail.com>
2024-04-13 18:40:16 +0300

    feat: create necessary dtos

---
+++ BACKEND/Core/codegather.Application/DTOs/JudgeResultDto.cs
@@ -0,0 +1,13 @@
+﻿namespace codegather.Application;
+
+public class JudgeResultDto
+{
+    public string stdout { get; set; } = "";
+    public string time { get; set; } = "";
+    public int memory { get; set; }
+    public string stderr { get; set; } = "";
+    public string token { get; set; } = "";
+    public string compile_output { get; set; } = "";
+    public string message { get; set; } = "";
+
+}
---
+++ BACKEND/Core/codegather.Application/DTOs/JudgeSubmissionDto.cs
@@ -0,0 +1,7 @@
+﻿namespace codegather.Application;
+
+public class JudgeSubmissionDto
+{
+    public string source_code { get; set; } = "";
+    public int language_id { get; set; }
+}
