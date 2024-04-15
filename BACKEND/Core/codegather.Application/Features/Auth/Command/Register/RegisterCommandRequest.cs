aa477ba2921a9b178bc2ea75718c98565e7d8c1c
yigitkalan <sametykalan@gmail.com>
2024-04-15 23:34:01 +0300

    feat: merge fixes from backend

--- BACKEND/Core/codegather.Application/Features/Auth/Command/Register/RegisterCommandRequest.cs
+++ BACKEND/Core/codegather.Application/Features/Auth/Command/Register/RegisterCommandRequest.cs
@@ -7,6 +7,5 @@ public class RegisterCommandRequest : IRequest<Unit>
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string ConfirmedPassword { get; set; }
 
 }
