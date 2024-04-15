e5c6996c25cf142a4d2ddd7ff659cb83126e551a
yigitkalan <sametykalan@gmail.com>
2024-04-15 23:23:56 +0300

    refactor: visual changes

--- FRONTEND/src/app/pages/login/components/login_form/LoginForm.tsx
+++ FRONTEND/src/app/pages/login/components/login_form/LoginForm.tsx
@@ -4,7 +4,7 @@ import InputField from "@/app/components/input_field/InputField";
 import {LoginUserDTO} from "@/app/models/LoginUserDTO";
 import {Login} from "@/app/services/AuthService";
 import Link from "next/link";
-import {redirect} from "next/navigation";
+import {useRouter} from "next/navigation";
 import {useRef, useState} from "react";
 import {useFormStatus} from "react-dom";
 import toast from "react-hot-toast";
@@ -12,6 +12,7 @@ import toast from "react-hot-toast";
 export default function LoginForm() {
   const {pending} = useFormStatus()
   const ref = useRef<HTMLFormElement>(null);
+  const router = useRouter();
 
   const handleSubmit = async (formData: FormData) => {
     const loginInfo: LoginUserDTO = {
@@ -19,9 +20,7 @@ export default function LoginForm() {
       password: formData.get("password") as string
     };
 
-    const toastId = toast.loading("Logging in...");
     const reponse = await Login(loginInfo);
-    toast.dismiss(toastId);
 
     ref.current?.reset();
 
@@ -30,16 +29,19 @@ export default function LoginForm() {
       return;
     }
 
-    redirect("/pages/ongoingCompetitions");
-  };
+    toast.success("Logged in successfully");
+    setTimeout(() => {
+      router.push("/pages/ongoingCompetitions")
+    }, 1000);
+  }
 
   return (
 
     <form ref={ref} action={handleSubmit}>
-      <div style={{margin: "0.5rem", padding: "0rem 3.75rem"}}>
+      <div style={{margin: "0.5rem", padding: "0rem 2.5rem"}}>
         <InputField type="text" name="email" label="email" required={true} />
       </div>
-      <div style={{margin: "0.5rem", padding: "0rem 3.75rem"}}>
+      <div style={{margin: "0.5rem", padding: "0rem 2.5rem"}}>
         <InputField
           type="password"
           name="password"
--- FRONTEND/src/app/pages/ongoingCompetitions/page.tsx
+++ FRONTEND/src/app/pages/ongoingCompetitions/page.tsx
@@ -5,9 +5,9 @@ export default async function OngoingCompetitions() {
     const competitions =  await GetAllCompetitions();
     return (
         <div className="h-100 bg-dark">
-            <h1 className="pt-5 container" style={{fontSize: "5rem"}}>ONGOING COMPETITIONS</h1>
+            <h1 className="pt-5 container text-white" style={{fontSize: "5rem"}}>ONGOING COMPETITIONS</h1>
             <div className="d-flex flex-row justify-content-evenly">
-                <div className="mt-5 ms-5 ps-5 d-flex flex-wrap  col-7">
+                <div className="mt-5 ms-5 d-flex flex-wrap justify-content-evenly  col-7">
                     {competitions.map((competition, index) => (
                         <CompetitionCard key={competition.id || index} competition={competition} />
                     ))}
