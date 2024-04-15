d5a41025cff5993a8d77176b188fa92125705ce8
yigitkalan <sametykalan@gmail.com>
2024-04-15 21:19:20 +0300

    feat: add custom font sizes and update login page

--- FRONTEND/src/app/pages/login/components/login_form/LoginForm.tsx
+++ FRONTEND/src/app/pages/login/components/login_form/LoginForm.tsx
@@ -3,12 +3,14 @@ import Button from "@/app/components/button/Button";
 import InputField from "@/app/components/input_field/InputField";
 import {LoginUserDTO} from "@/app/models/LoginUserDTO";
 import {Login} from "@/app/services/AuthService";
+import Link from "next/link";
 import {redirect} from "next/navigation";
 import {useRef, useState} from "react";
+import {useFormStatus} from "react-dom";
 import toast from "react-hot-toast";
 
 export default function LoginForm() {
-  const [loggignIn, setLoggingIn] = useState(false);
+  const {pending} = useFormStatus()
   const ref = useRef<HTMLFormElement>(null);
 
   const handleSubmit = async (formData: FormData) => {
@@ -17,10 +19,8 @@ export default function LoginForm() {
       password: formData.get("password") as string
     };
 
-    setLoggingIn(true);
     const toastId = toast.loading("Logging in...");
     const reponse = await Login(loginInfo);
-    setLoggingIn(false);
     toast.dismiss(toastId);
 
     ref.current?.reset();
@@ -37,21 +37,26 @@ export default function LoginForm() {
 
     <form ref={ref} action={handleSubmit}>
       <div style={{margin: "0.5rem", padding: "0rem 3.75rem"}}>
-        <InputField type="text" name="email" label="email" />
+        <InputField type="text" name="email" label="email" required={true} />
       </div>
       <div style={{margin: "0.5rem", padding: "0rem 3.75rem"}}>
         <InputField
           type="password"
           name="password"
           label="Password"
+          required={true}
         />
       </div>
-
-      <div className="m-3">
-        <button className="btn btn-dark m-3 p-2" disabled={loggignIn}>
-
-          <span>Login</span>
+      <div className="m-3 mb-0">
+        <button className="btn btn-dark m-3 mb-0 p-2" disabled={pending}>
+          <span className="fs-5 p-2" >Login</span>
         </button>
+      </div>loginform
+      <div className="mb-3">
+        <Link href="/pages/register" className="mb-3 fs-6 text-decoration-none">
+          Create an account
+        </Link>
+
       </div>
 
     </form>
--- FRONTEND/src/app/pages/login/page.tsx
+++ FRONTEND/src/app/pages/login/page.tsx
@@ -7,22 +7,21 @@ export default function LoginPage() {
 
   return (
     <div className="flex-col">
-      <div className="d-flex justify-content-center align-items-center text-center vh-100 bg-dark row-10">
-        <div className="d-flex flex-row align-items-center justify-content-center h-75">
-          <div className="col me-5">
-            <h1 className="display-1 fw-bold text-white">Welcome </h1>
-            <h1 className="display-1 fw-bold m-2 p-2 text-end text-white">
-              Back{" "}
+      <div className="d-flex justify-content-center align-items-center text-center vh-100 bg-dark">
+        <div className="d-flex flex-row align-items-center justify-content-evenly h-75 col-10">
+          <div className="">
+            <h1 className="display-1 fw-bold text-white fs-l" >Welcome </h1>
+            <h1 className="display-1 fw-bold m-2 p-2 text-end text-white ">
+              Back
             </h1>
           </div>
-          <div className="col h-75">
+          <div className="col-3">
             <Card >
-              <div className="pt-5">
-                <h2 className="fw-bold fs-2 mb-3">Login</h2>
-                <LoginForm/>
+              <div className="pt-5 d-flex flex-column justify-content-center h-100">
+                <h2 className="fw-bold fs-2 mb-3 text-white">Login</h2>
+                <LoginForm />
               </div>
             </Card>
-
           </div>
         </div>
       </div>
