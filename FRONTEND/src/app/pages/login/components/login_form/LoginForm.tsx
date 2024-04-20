a2a63c29c2c943ff0ea0d76de01381076772d981
diclebahceli <diclebahceli24@gmail.com>
2024-04-20 15:49:42 +0300

    feat: save role to context

--- FRONTEND/src/app/pages/login/components/login_form/LoginForm.tsx
+++ FRONTEND/src/app/pages/login/components/login_form/LoginForm.tsx
@@ -1,49 +1,62 @@
 "use client";
 import InputField from "@/app/components/input_field/InputField";
-import {LoginUserDTO} from "@/app/models/LoginUserDTO";
-import {Login} from "@/app/services/AuthService";
+import { AuthContext } from "@/app/contexts/AuthContext";
+import { Claims } from "@/app/models/Claims";
+import { LoginUserDTO } from "@/app/models/LoginUserDTO";
+import { Login } from "@/app/services/AuthService";
+import { getWithExpiry } from "@/app/utils/StorageGetter";
+import { Console } from "console";
+import { jwtDecode } from "jwt-decode";
 import Link from "next/link";
-import {useRouter} from "next/navigation";
-import {useRef} from "react";
-import {useFormStatus} from "react-dom";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useRef } from "react";
+import { useFormStatus } from "react-dom";
 import toast from "react-hot-toast";
 
 export default function LoginForm() {
-  const {pending} = useFormStatus()
+  const { pending } = useFormStatus();
   const ref = useRef<HTMLFormElement>(null);
   const router = useRouter();
+  const authContext = useContext(AuthContext);
 
   const handleSubmit = async (formData: FormData) => {
     const loginInfo: LoginUserDTO = {
       email: formData.get("email") as string,
-      password: formData.get("password") as string
+      password: formData.get("password") as string,
     };
 
     const reponse = await Login(loginInfo);
-
 
     ref.current?.reset();
 
     if (reponse.error) {
       toast.error(reponse.error);
       return;
+    } else {
+      toast.success("Logged in successfully");
+      if (authContext) {
+        const { loginn } = authContext;
+        const token = getWithExpiry("accessToken");
+        if (token != null) {
+          const decodedToken = jwtDecode(token) as Claims;
+          loginn(decodedToken.role);
+        }
+      } else {
+        console.error("Auth context is not available");
+      }
+
+      setTimeout(() => {
+        router.replace("/pages/ongoingCompetitions");
+      }, 1000);
     }
-
-
-    toast.success("Logged in successfully");
-
-    setTimeout(() => {
-      router.push("/pages/ongoingCompetitions")
-    }, 1000);
-  }
+  };
 
   return (
-
     <form ref={ref} action={handleSubmit}>
-      <div style={{margin: "0.5rem", padding: "0rem 2.5rem"}}>
+      <div style={{ margin: "0.5rem", padding: "0rem 2.5rem" }}>
         <InputField type="text" name="email" label="email" required={true} />
       </div>
-      <div style={{margin: "0.5rem", padding: "0rem 2.5rem"}}>
+      <div style={{ margin: "0.5rem", padding: "0rem 2.5rem" }}>
         <InputField
           type="password"
           name="password"
@@ -53,16 +66,15 @@ export default function LoginForm() {
       </div>
       <div className="m-3">
         <button className="btn btn-dark m-3 mb-0 p-2" disabled={pending}>
-          <span className="fs-5 p-2" >Login</span>
+          <span className="fs-5 p-2">Login</span>
         </button>
       </div>
+      loginform
       <div className="mb-3">
         <Link href="/pages/register" className="mb-3 fs-6 text-decoration-none">
           Create an account
         </Link>
-
       </div>
-
     </form>
   );
 }
