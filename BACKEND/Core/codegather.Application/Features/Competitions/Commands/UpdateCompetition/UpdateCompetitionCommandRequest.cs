ee33aa0f2a75d6d81fddb721c093e63fadaa7e0e
diclebahceli <diclebahceli24@gmail.com>
2024-04-28 16:47:45 +0300

    fix: update namings of feature

--- BACKEND/Core/codegather.Application/Features/Competitions/Commands/UpdateCompetition/UpdateCompetitionCommandRequest.cs
+++ BACKEND/Core/codegather.Application/Features/Competitions/Commands/UpdateCompetition/UpdateCompetitionCommandRequest.cs
@@ -7,6 +7,6 @@ public class UpdateCompetitionCommandRequest : IRequest<Unit>
     public Guid Id { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
-    public DateTime StartTime { get; set; }
-    public DateTime EndTime { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
 }
--- BACKEND/Core/codegather.Application/Features/Competitions/Commands/UpdateCompetition/UpdateCompetitionCommandValidator.cs
+++ BACKEND/Core/codegather.Application/Features/Competitions/Commands/UpdateCompetition/UpdateCompetitionCommandValidator.cs
@@ -8,10 +8,10 @@ public class UpdateCompetitionCommandValidator : AbstractValidator<UpdateCompeti
         RuleFor(x => x.Id).NotNull().WithMessage("Id must be valid");
         RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required");
         RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
-        RuleFor(x => x.StartTime).NotEmpty().WithMessage("StartDate is required");
-        RuleFor(x => x.StartTime).GreaterThan(DateTime.Now).WithMessage("StartDate must be after today");
-        RuleFor(x => x.EndTime).GreaterThan(DateTime.Now).WithMessage("EndTime must be after today");
-        RuleFor(x => x.EndTime).NotEmpty().WithMessage("EndDate is required");
-        RuleFor(x => x.EndTime).GreaterThan(x => x.StartTime).WithMessage("EndTime must be after StartDate");
+        RuleFor(x => x.StartDate).NotEmpty().WithMessage("StartDate is required");
+        RuleFor(x => x.StartDate).GreaterThan(DateTime.Now).WithMessage("StartDate must be after today");
+        RuleFor(x => x.EndDate).GreaterThan(DateTime.Now).WithMessage("EndTime must be after today");
+        RuleFor(x => x.EndDate).NotEmpty().WithMessage("EndDate is required");
+        RuleFor(x => x.EndDate).GreaterThan(x => x.StartDate).WithMessage("EndTime must be after StartDate");
     }
 }
