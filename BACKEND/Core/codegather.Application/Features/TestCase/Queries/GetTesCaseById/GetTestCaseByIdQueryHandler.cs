4bdfdce6705cd738dc475334dba09bbaf22abe9a
diclebahceli <diclebahceli24@gmail.com>
2024-04-30 13:39:07 +0300

    feat: add GetTestCaseById feature

---
+++ BACKEND/Core/codegather.Application/Features/TestCase/Queries/GetTesCaseById/GetTestCaseByIdQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using codegather.Application.Interfaces.AutoMapper;
+using codegather.Domain;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace codegather.Application;
+
+public class GetTestCaseByIdQueryHandler : BaseHandler, IRequestHandler<GetTestCaseByIdQueryRequest, GetTestCaseByIdQueryResponse>
+{
+    public GetTestCaseByIdQueryHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor) : base(mapper, unitOfWork, httpContextAccessor)
+    {
+    }
+
+    public async Task<GetTestCaseByIdQueryResponse> Handle(GetTestCaseByIdQueryRequest request, CancellationToken cancellationToken)
+    {
+        var testCase = await unitOfWork.GetReadRepository<TestCase>().GetAsync(predicate: q => q.Id == request.Id && q.IsDeleted == false) ?? throw new Exception("No such test case found");
+
+        return new GetTestCaseByIdQueryResponse
+        {
+            TestCase = mapper.Map<TestCase>(testCase)
+        };
+    }
+}
