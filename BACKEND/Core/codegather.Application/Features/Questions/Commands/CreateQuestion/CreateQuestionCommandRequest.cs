b59f708cdadad9dd7855c5c2f77c8d1dcd6aa863
diclebahceli <diclebahceli24@gmail.com>
2024-04-30 14:09:01 +0300

    fix: update the CreateQuestionCommandRequest

--- BACKEND/Core/codegather.Application/Features/Questions/Commands/CreateQuestion/CreateQuestionCommandRequest.cs
+++ BACKEND/Core/codegather.Application/Features/Questions/Commands/CreateQuestion/CreateQuestionCommandRequest.cs
@@ -1,8 +1,9 @@
 using MediatR;
 
 namespace codegather.Application;
-public class CreateQuestionCommandRequest: IRequest<CreateQuestionCommandResponse>
+public class CreateQuestionCommandRequest : IRequest<CreateQuestionCommandResponse>
 {
     public Guid CompetitionId { get; set; }
     public string Description { get; set; }
+    public string StarterCode { get; set; }
 }
