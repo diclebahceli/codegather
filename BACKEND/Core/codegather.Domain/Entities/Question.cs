227fa1e8d0d73f2038f0a9b88c61af7effb14564
diclebahceli <diclebahceli24@gmail.com>
2024-04-30 12:10:58 +0300

    feat: update the QuestionDto

--- BACKEND/Core/codegather.Domain/Entities/Question.cs
+++ BACKEND/Core/codegather.Domain/Entities/Question.cs
@@ -1,14 +1,15 @@
 namespace codegather.Domain;
-public class Question: EntityBase
+public class Question : EntityBase
 {
-    public Competition Competition {get; set;}
-    public Guid CompetitionId {get; set;}
-    public ICollection<Submission> Submissions {get; set;}
-    public string Description {get; set;}
-    public string TestCases {get; set;}
+    public Competition Competition { get; set; }
+    public Guid CompetitionId { get; set; }
+    public ICollection<Submission> Submissions { get; set; }
+    public string Description { get; set; }
+    public ICollection<TestCase> TestCases { get; set; }
 
-    public Question(){ }
-    public Question(Competition competition, string description, string testCases){
+    public Question() { }
+    public Question(Competition competition, string description, ICollection<TestCase> testCases)
+    {
         Competition = competition;
         Description = description;
         TestCases = testCases;
