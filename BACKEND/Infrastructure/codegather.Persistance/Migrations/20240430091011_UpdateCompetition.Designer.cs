aeebdac620278c79816ab006952c480f5e79468f
yigitkalan <sametykalan@gmail.com>
2024-04-30 12:22:47 +0300

    feat: update migrations

--- BACKEND/Infrastructure/codegather.Persistance/Migrations/20240430091011_UpdateCompetition.Designer.cs
+++ BACKEND/Infrastructure/codegather.Persistance/Migrations/20240430091011_UpdateCompetition.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using codegather.Persistance;
 
@@ -10,9 +11,11 @@ using codegather.Persistance;
 namespace codegather.Persistance.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240430091011_UpdateCompetition")]
+    partial class UpdateCompetition
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.5");
