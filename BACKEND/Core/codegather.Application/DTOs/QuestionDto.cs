db14c03a6c6b0349315f02dd21de997a263aa3f5
diclebahceli <diclebahceli24@gmail.com>
2024-04-30 12:38:40 +0300

    fix: update the question dto

--- BACKEND/Core/codegather.Application/DTOs/QuestionDto.cs
+++ BACKEND/Core/codegather.Application/DTOs/QuestionDto.cs
@@ -6,6 +6,5 @@ public class QuestionDto
     public Guid Id { get; set; }
     public Guid CompetitionId { get; set; }
     public string Description { get; set; }
-    public ICollection<TestCase> TestCases { get; set; }
 
 }
