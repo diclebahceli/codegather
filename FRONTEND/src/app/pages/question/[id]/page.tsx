eb9b9606ed639b836f5f1a202ac4aa8ea7896555
yigitkalan <sametykalan@gmail.com>
2024-05-15 17:08:48 +0300

    feat: finish run function on question page

--- FRONTEND/src/app/pages/question/[id]/page.tsx
+++ FRONTEND/src/app/pages/question/[id]/page.tsx
@@ -51,12 +51,12 @@ export default function EditorPage({params}: {params: {id: string}}) {
         , code: code, languageId: 71
       };
 
-      const result = await RunCode(runReq)
-      if (result.error || !result.data) {
-        toast.error(result.error)
+      const res = await RunCode(runReq)
+      if (res.error || !res.data) {
+        toast.error(res.error)
         return;
       }
-      setResult(result.data);
+      setResult(res.data);
     } catch (e: Error | any) {
       toast.error(e.message);
     }
@@ -90,16 +90,46 @@ export default function EditorPage({params}: {params: {id: string}}) {
           </div>
           <div className="flex-grow-1 mt-2">
             <Card>
-              <div className="d-flex flex-column  p-5 pt-2 pe-4">
+              <div className="d-flex flex-column  p-5 pt-2 pe-4 h-100">
                 <div className="d-flex flex-row ">
                   <div className="text-white fs-4 align-self-start ">Results</div>
                   <div className="flex-grow-1"></div>
                   <button onClick={handleRun} className="btn btn-dark fs-5 me-3">Run</button>
                   <button className="btn btn-green fs-5">Submit</button>
                 </div>
-                <div className="d-flex flex-column mt-3">
-                  <div className="text-white fs-5">Output: {result.stdout}</div>
+                <div className="my-2">
+                  {result.stderr && <div className="text-white fw-bold ">Compile Error:
+                    <span className="text-danger opacity-75"> {result.stderr}</span></div>}
+                  {result.stdout &&
+                    <div className="d-flex flex-column h-100">
+                      <div className="d-flex flex-row">
+                        <div className="text-white me-3 fw-bold"> Input :
+                          <span className="mx-2 fw-normal opacity-75">{question.testCases[0].input}</span>
+                        </div>
+                        <div className="text-white me-3 fw-bold"> Expected Output :
+                          <span className=" mx-2 fw-normal opacity-75">
+                            {question.testCases[0].output}
+                          </span>
+                        </div>
+                      </div>
+                      {result.stderr && <div className="text-white"> Compile Error: {result.stderr} </div>}
+
+                      <div className="py-3">
+                        {question.testCases[0].output != result.stdout && !result.stderr ?
+                          <div className="text-danger fw-bold"> Your Output: {result.stdout} </div>
+                          :
+                          <div className="text-green fw-bold"> Your Output:
+                            <span className="mx-2 fw-normal opacity-75">
+                              {result.stdout}
+                            </span>
+                          </div>
+                        }
+                      </div>
+                    </div>
+                  }
+
                 </div>
+
               </div>
             </Card>
           </div>
