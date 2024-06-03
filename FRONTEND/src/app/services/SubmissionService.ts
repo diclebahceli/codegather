d3280e5b5fcec036f3adc88f43bfea0ba2c80bb9
yigitkalan <sametykalan@gmail.com>
2024-06-03 16:31:05 +0300

    feat/front: add new submission service methods

--- FRONTEND/src/app/services/SubmissionService.ts
+++ FRONTEND/src/app/services/SubmissionService.ts
@@ -28,12 +28,48 @@ export async function SubmitCode(requestData: RunRequest): Promise<{data: Submis
       return {data: null, error: ExtractErrorMessage(response)};
     }
     const submission = response.data.submission as Submission;
-    if(!submission.errorFree){
+    if (!submission.errorFree) {
       submission.errorMessage = response.data.stderr;
     }
+    submission.score = Number(submission.score.toFixed(2));
     return {data: response.data.submission as Submission, error: null};
   } catch (e: Error | any) {
     return {data: null, error: ExtractErrorMessage(e)};
   }
 }
 
+export async function GetUsersSubmissionsForQuestion(questionId: string, userId: string):
+  Promise<{data: Submission[] | null; error: string | null}> {
+  try {
+    const response = await axios.get(`${submissionEndPoint}/GetByUserAndQuestionId/${userId}/${questionId}`);
+    if (response.status != 200) {
+      return {data: null, error: ExtractErrorMessage(response)};
+    }
+    const submissions = response.data.submissions as Submission[];
+    submissions.forEach(submission => {
+      submission.score = Number(submission.score.toFixed(2));
+    });
+    return {data: response.data.submissions as Submission[], error: null};
+  }
+  catch (e: Error | any) {
+    return {data: null, error: ExtractErrorMessage(e)};
+  }
+
+}
+
+export async function GetLastSubmissionForQuestion(questionId: string, userId: string):
+  Promise<{data: Submission | null; error: string | null}> {
+  try {
+    const response = await axios.get(`${submissionEndPoint}/GetLastSubmission/${userId}/${questionId}`);
+    if (response.status != 200) {
+      return {data: null, error: ExtractErrorMessage(response)};
+    }
+    const submission = response.data.submission as Submission;
+    submission.score =  Number(submission.score.toFixed(2));
+
+    return {data: submission, error: null};
+  }
+  catch (e: Error | any) {
+    return {data: null, error: ExtractErrorMessage(e)};
+  }
+}
